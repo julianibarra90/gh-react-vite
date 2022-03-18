edbde43792c394e3a98239918748af2b56865b00
julianibarra90 <anner.smith90@gmail.com>
2022-03-18 04:07:36 -0500

    FIX BUG

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://julianibarra90.github.io/',
+  base: 'https://julianibarra90.github.io/gh-react-vite',
   plugins: [react()]
 })
